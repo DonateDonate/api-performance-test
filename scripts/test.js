631847605e4f2dfc06e3d1b712ebae85dda76066
kimsanhaa <ttksgk@gmail.com>
2024-10-01 23:43:03 +0900

    Feat: docker compose version 삭제

--- scripts/test.js
+++ scripts/test.js
@@ -4,7 +4,7 @@ import ws from 'k6/ws';
 
 export const options = {
     vus: 1,
-    duration: '1s'
+    duration: '5s'
 };
 
 const HTTP_BASE_URL = 'http://sanha-api.shop:8080';
@@ -53,38 +53,31 @@ const room = {
         });
     },
 
-    socketAll: function(url, token, message,) {
+    socketAll: function(url, token, message) {
         const params = {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': token
             }
         };
+        
         ws.connect(url, params, function(socket) {
             socket.on('open', function open() {
                 socket.send(JSON.stringify(message));
-
-                let lat = 37.2073460596323;
-                let lng = 127.453777119874;
-                const stepSize = 0.01;
-                for(let i = 0; i<200; i++){
-                    lat = lat + stepSize;
-                    lng = lng + stepSize;
-                    socket.send(JSON.stringify(room.makeSocketMessage(2,lat,lng,200)))   
+                
+                let lat = 37.566127;
+                let lng = 127.042965;
+                const stepSize = 0.02;
+            
+                for (let i = 0; i < 150; i++) {
+                    
+                    lat += stepSize;
+                    lng += stepSize;
+                
+                    socket.send(JSON.stringify(room.makeSocketMessage(2, lat, lng, 200)));
                 }
-          
-                // socket.setInterval(function timeout() {
-                //     let lat = 37.5681501121823;
-                //     let lng = 126.9893055655;
-                //     const stepSize = 0.0001;
-                //     for(let i = 0; i<200; i++){
-                //         lat = lat + stepSize;
-                //         lng = lng + stepSize;
-                //         socket.send(JSON.stringify(room.makeSocketMessage(2,lat,lng,200)))   
-                //     }
-                // }, 1000);
-              });
-            socket.on('message', (data) => console.log('Message received: ', data));
+            });
+    
             socket.on('close', () => console.log('disconnected'));
         });
     },
@@ -108,17 +101,9 @@ const room = {
 export default function () {
     console.log('Function executed');
     
-    // Uncomment these lines if needed
-     const accessToken_1 = auth.login("01012345678", "1234", HTTP_BASE_URL);
-     //const accessToken_2 = auth.login("01075699952", "1234", HTTP_BASE_URL);
-    //const createRes = room.create(37, 42, "room_k6_1", "2024-09-27 18:00", accessToken);
-    // console.log(createRes.shareCode);
-    // console.log(createRes.roomSeq);
-
-    const createMessage = room.makeSocketMessage(0, 37, 42, 300);
-    //const joinMessage = room.makeSocketMessage(1, 37, 42, 300);
- 
+    const accessToken_1 = auth.login("01012345678", "1234", HTTP_BASE_URL);
+    
+    const createMessage = room.makeSocketMessage(1, 37, 42, 300);
+    
     room.socketAll(WS_BASE_URL, accessToken_1, createMessage);
-    //room.socketConnect(WS_BASE_URL, accessToken_2);
-
 }
